108f1c1d9887a6ea3df77c9f8f4fb61f0bbee307
Blake Crosby <ptsouth97@gmail.com>
2020-02-11 03:36:12 +0000

    initial commit

---
+++ motor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+
+# l293d documentation from:  https://github.com/jmsv/l293d
+
+def main():
+	''' main function'''
+
+	import l293d.driver as l293d
+
+	# Motor 1 uses Pin 22, Pin 18, Pin 16
+	motor = l293d.DC(22,18,16)
+	motor.clockwise(duration=3, speed=25)
+
+	# Run the motors so visible
+	#for i in range(0,1500):
+	#	motor.clockwise()
+
+	l293d.cleanup()
+
+
+if __name__ == '__main__':
+	main()
